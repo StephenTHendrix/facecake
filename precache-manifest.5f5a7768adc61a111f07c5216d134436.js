fcee07f9a674e063378ae61e732b70b5b0ca751d
StephenTHendrix <stephenthendrix@gmail.com>
2020-08-09 21:39:09 -0500

    Updates

---
+++ precache-manifest.5f5a7768adc61a111f07c5216d134436.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894ab6f5ab32216183b4ea6c5aa35a44",
+    "url": "/facecake/index.html"
+  },
+  {
+    "revision": "e696bdc20c3d5f674f7c",
+    "url": "/facecake/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1c15fde948a347b94fc1",
+    "url": "/facecake/static/js/2.d6c7a77e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/facecake/static/js/2.d6c7a77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e696bdc20c3d5f674f7c",
+    "url": "/facecake/static/js/main.2e23eff9.chunk.js"
+  },
+  {
+    "revision": "f55f2cf64389ac517598",
+    "url": "/facecake/static/js/runtime-main.45518ebe.js"
+  },
+  {
+    "revision": "c5929e50817247187dd4c6cf67168709",
+    "url": "/facecake/static/media/att.c5929e50.png"
+  },
+  {
+    "revision": "b24b2dd2baa9b991325667ef5eb1efd0",
+    "url": "/facecake/static/media/bellhops.b24b2dd2.png"
+  },
+  {
+    "revision": "8c1add94f801f3b211b2d15ce80f4801",
+    "url": "/facecake/static/media/facebook.8c1add94.svg"
+  },
+  {
+    "revision": "b1d2b40c62bd971dde5853b300e6bfa3",
+    "url": "/facecake/static/media/fallingLine.b1d2b40c.svg"
+  },
+  {
+    "revision": "f28bdeedf387dbc08b171b19e12bf0e6",
+    "url": "/facecake/static/media/heroToServices.f28bdeed.svg"
+  },
+  {
+    "revision": "231943cef6a46d945790b4d55c019229",
+    "url": "/facecake/static/media/instagram.231943ce.svg"
+  },
+  {
+    "revision": "9ee4f85e094eda4d57b571256a66ef37",
+    "url": "/facecake/static/media/lemonade.9ee4f85e.png"
+  },
+  {
+    "revision": "69ca2f938204dbd3aa7279c26cd6bda5",
+    "url": "/facecake/static/media/privacyToSplit.69ca2f93.svg"
+  },
+  {
+    "revision": "0c276ddc006844e8781465b158ee9d40",
+    "url": "/facecake/static/media/rightArrow.0c276ddc.svg"
+  },
+  {
+    "revision": "0b79eefe75f1f9b2f1eed4b8983f069b",
+    "url": "/facecake/static/media/risingLine.0b79eefe.svg"
+  },
+  {
+    "revision": "371e95e590f37c0cc84ce547121406e6",
+    "url": "/facecake/static/media/spectrum.371e95e5.png"
+  },
+  {
+    "revision": "ef2c2650e9f6ab1adf9f85493ad25695",
+    "url": "/facecake/static/media/splitToFooter.ef2c2650.svg"
+  },
+  {
+    "revision": "f91e88e6c6ee8abf26fcb5be6e08cf3c",
+    "url": "/facecake/static/media/twitter.f91e88e6.svg"
+  },
+  {
+    "revision": "9a40441684c1d8c427042a8209b7e17c",
+    "url": "/facecake/static/media/vivint.9a404416.png"
+  },
+  {
+    "revision": "328390ff8d8681de839c720c92283300",
+    "url": "/facecake/static/media/whiteRightArrow.328390ff.svg"
+  },
+  {
+    "revision": "cd6a666c247786242c41ef7f40596041",
+    "url": "/facecake/static/media/youtube.cd6a666c.svg"
+  }
+]);
